30ddf453a409390825709e6ab0c65ef61d43c9df
maxyee <eyaminkhan00@gmail.com>
2021-05-24 21:01:58 +0600

    clean up the react project

---
+++ src/firebase/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCJtv27ZPTBDo4dK-CwiAEsohlL2BukztI",
+    authDomain: "whatsapp-clone-43df6.firebaseapp.com",
+    projectId: "whatsapp-clone-43df6",
+    storageBucket: "whatsapp-clone-43df6.appspot.com",
+    messagingSenderId: "326895346554",
+    appId: "1:326895346554:web:c9a58c3bab6fb8fe9cacc3",
+    measurementId: "G-N2LPT490Y5"
+};
