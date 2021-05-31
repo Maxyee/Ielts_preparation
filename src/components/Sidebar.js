df416851e3f6b60f6381e79e993ff8f133a31cf6
maxyee <eyaminkhan00@gmail.com>
2021-06-01 00:52:06 +0600

    finished making sidebar search section

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,6 +4,7 @@ import DonutLargeIcon from '@material-ui/icons/DonutLarge';
 import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import { Avatar, IconButton } from "@material-ui/core";
+import { SearchOutlined } from "@material-ui/icons";
 
 import '../styles/Sidebar.css'
 
@@ -28,11 +29,15 @@ function Sidebar() {
             </div>
 
             <div className="sidebar__search">
-
+                <div className="sidebar__searchContainer">
+                    <SearchOutlined />
+                    <input placeholder="Search or start new chat" type="text" />
+                </div>
             </div>
 
             <div className="sidebar__chats">
-
+                <SidebarChat />
+                <h1>Sidebar Chat</h1>
             </div>
         </div>
     )
