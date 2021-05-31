166f1b71638f93c3403ab0f53a5fd251371f7616
maxyee <eyaminkhan00@gmail.com>
2021-06-01 00:15:20 +0600

    finished the sidebar header component work

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="app">
       <div className="app__body">
         {/* SideBar */}
+        <Sidebar />
         {/* Chat */}
       </div>
     </div>
