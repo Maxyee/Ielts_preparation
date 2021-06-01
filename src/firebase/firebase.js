6a323547b6665fd1c1447df9629aad1b45d43f68
maxyee <eyaminkhan00@gmail.com>
2021-06-02 01:49:26 +0600

    code for firebase database connection

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,3 +1,5 @@
+import firebase from "firebase";
+
 const firebaseConfig = {
     apiKey: "AIzaSyCJtv27ZPTBDo4dK-CwiAEsohlL2BukztI",
     authDomain: "whatsapp-clone-43df6.firebaseapp.com",
@@ -7,3 +9,11 @@ const firebaseConfig = {
     appId: "1:326895346554:web:c9a58c3bab6fb8fe9cacc3",
     measurementId: "G-N2LPT490Y5"
 };
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
